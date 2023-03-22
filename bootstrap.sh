febd7fc71dcb168aafa97383a9fc649db56d8c9a
vivianeflowt <vivianeflowt@gmail.com>
2023-03-22 20:41:37 -0300

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,6 @@ sudo docker stop $(sudo docker ps -a -q) 2>/dev/null
 sudo docker rm $(sudo docker ps -a -q) 2>/dev/null
 sudo docker volume rm $(sudo docker volume ls -qf dangling=true) 2>/dev/null
 sudo docker network rm $(sudo docker network ls -q) 2>/dev/null
-wait
 clear
 
 node .docker/bootstrap.js
